28f77e7bad639255af581e31e3bef74dbaa579d6
zhangjie <jie.zhang@talend.com>
2019-11-26 17:34:43 +0800

    feat(TUP-24651)Add choice between log4j and log4j2 in project settings

--- main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/utils/Log4jUtil.java
+++ main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/utils/Log4jUtil.java
@@ -1,10 +1,9 @@
 package org.talend.core.repository.utils;
 
-import org.talend.core.PluginChecker;
-
 public class Log4jUtil {
 
     public static boolean isEnable() {
-        return PluginChecker.isTIS();
+//        return PluginChecker.isTIS();
+        return true;
     }
 }
