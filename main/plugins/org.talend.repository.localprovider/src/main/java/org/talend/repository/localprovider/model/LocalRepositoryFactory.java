16ff056d1aac073a22ded1cb4d553fbb30ffd7fc
jding-tlnd <jding@talend.com>
2019-11-27 18:31:12 +0800

    fix(TUP-23082)[Performance] import specific job takes more than 44 mins and still going on https://jira.talendforge.org/browse/TUP-23082 For batch delete, save project many times during delete process change to save once at end

--- main/plugins/org.talend.repository.localprovider/src/main/java/org/talend/repository/localprovider/model/LocalRepositoryFactory.java
+++ main/plugins/org.talend.repository.localprovider/src/main/java/org/talend/repository/localprovider/model/LocalRepositoryFactory.java
@@ -138,6 +138,7 @@ import org.talend.core.model.properties.Status;
 import org.talend.core.model.properties.TDQItem;
 import org.talend.core.model.properties.User;
 import org.talend.core.model.properties.ValidationRulesConnectionItem;
+import org.talend.core.model.relationship.RelationshipItemBuilder;
 import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.core.model.repository.Folder;
 import org.talend.core.model.repository.IRepositoryContentHandler;
@@ -1715,11 +1716,27 @@ public class LocalRepositoryFactory extends AbstractEMFRepositoryFactory impleme
     @Override
     public void deleteObjectPhysical(Project project, IRepositoryViewObject objToDelete, String version,
             boolean fromEmptyRecycleBin, boolean isDeleteOnRemote) throws PersistenceException {
+        deleteObjectPhysical(project, objToDelete, version, fromEmptyRecycleBin, isDeleteOnRemote, false);
+    }
+
+    /**
+     * 
+     * @param project
+     * @param objToDelete
+     * @param version
+     * @param fromEmptyRecycleBin
+     * @param isDeleteOnRemote
+     * @param aviodSave for batch delete, save later.
+     * @throws PersistenceException
+     */
+    protected void deleteObjectPhysical(Project project, IRepositoryViewObject objToDelete, String version,
+            boolean fromEmptyRecycleBin, boolean isDeleteOnRemote, boolean aviodSave) throws PersistenceException {
         if ("".equals(version)) { //$NON-NLS-1$
             version = null; // for all version
         }
         // can only delete in the main project
         List<IRepositoryViewObject> allVersionToDelete = getAllVersion(project, objToDelete.getId(), false);
+        RelationshipItemBuilder relationsBuilder = RelationshipItemBuilder.getInstance();
 
         for (IRepositoryViewObject currentVersion : allVersionToDelete) {
             String currentVersionValue = currentVersion.getVersion();
@@ -1734,7 +1751,13 @@ public class LocalRepositoryFactory extends AbstractEMFRepositoryFactory impleme
                     Project currentProject = ProjectManager.getInstance().getCurrentProject();
                     if (coreSerivce.isAlreadyBuilt(project) && currentProject != null) {
                         if (currentVersion.getProperty() != null) {
-                            coreSerivce.removeItemRelations(currentVersion.getProperty().getItem());
+                            // coreSerivce.removeItemRelations(currentVersion.getProperty().getItem());
+                            boolean originalStatus = relationsBuilder.isAutoSave();
+                            if (aviodSave) {
+                                relationsBuilder.setAutoSave(false);
+                            }
+                            relationsBuilder.removeItemRelations(currentVersion.getProperty().getItem());
+                            relationsBuilder.setAutoSave(originalStatus);
                         }
                     }
                 }
@@ -1777,7 +1800,7 @@ public class LocalRepositoryFactory extends AbstractEMFRepositoryFactory impleme
         if (!allVersionToDelete.isEmpty()) {
             RecycleBinManager.getInstance().removeFromRecycleBin(project, allVersionToDelete.get(0).getProperty().getItem());
         }
-        if (!fromEmptyRecycleBin) {
+        if (!fromEmptyRecycleBin && !aviodSave) {
             saveProject(project);
         }
     }
@@ -1786,8 +1809,10 @@ public class LocalRepositoryFactory extends AbstractEMFRepositoryFactory impleme
     public void batchDeleteObjectPhysical(Project project, List<IRepositoryViewObject> objToDeleteList, boolean isDeleteOnRemote)
             throws PersistenceException {
         for (IRepositoryViewObject object : objToDeleteList) {
-            deleteObjectPhysical(project, object, null, isDeleteOnRemote);
+            deleteObjectPhysical(project, object, null, false, isDeleteOnRemote, true);
         }
+        RelationshipItemBuilder relationsBuilder = RelationshipItemBuilder.getInstance();
+        relationsBuilder.saveRelations();
     }
 
     @Override
